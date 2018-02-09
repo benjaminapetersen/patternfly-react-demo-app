26f3acc96bbf18631a7139a7d19b489f1def023b
benjaminapetersen <ben@benjaminapetersen.me>
2018-02-08 23:54:38 -0500

    Articles experimentation

--- src/pages/Archives.js
+++ src/pages/Archives.js
@@ -3,8 +3,30 @@ import { Link } from 'react-router-dom';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { EmptyState } from '../components/EmptyState/EmptyState';
 
+import Article from '../components/Article/Article';
+
 class ArchivesPage extends React.Component<RouteComponentProps<any>, {}> {
   render() {
+    const articles = [
+      {title: 'Udon Noodles', url: 'archives/noodles?type=udon'},
+      {title: 'Soba Noodles', url: 'archives/noodles?type=soba'},
+      {title: 'Wonton Noodles', url: 'archives/noodles?type=wonton'},
+      {title: 'Pad Thai', url: 'archives/noodles?type=padthai'},
+      {title: 'Lo Mein', url: 'archives/noodles?type=lomein'}
+    ].map((article, i) => <li><Article key={i} {...article} /></li> );
+
+    const randomStuff = [
+      'Random #1',
+      'Random #2',
+      'Random #3'
+    ];
+
+    const randomThing = randomStuff[
+      Math.round( Math.random() * (randomStuff.length -1) )
+    ];
+
+    console.log('articles', articles);
+
     return (
       <div className="container-fluid container-pf-nav-pf-vertical">
         <div className="row">
@@ -16,10 +38,9 @@ class ArchivesPage extends React.Component<RouteComponentProps<any>, {}> {
         </div>
         <div className="row">
           <div className="col-md-12">
+            <div>{randomThing}</div>
             <ul>
-              <li>
-                <Link to="archives/noodles?type=udon">Udon Noodles!</Link>
-              </li>
+              {articles}
             </ul>
           </div>
         </div>
